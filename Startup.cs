8efae7f43b6977bfb9e0c76518bae6b285d8b870
kennergf <kenner.g.figueiredo@gmail.com>
2021-06-22 20:09:01 +0100

    Changed to use SQL Server

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace GarageManagementSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
