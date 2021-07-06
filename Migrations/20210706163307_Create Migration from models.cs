137dc095a6c208cdc82b4301909b7800fd18dd98
kennergf <kenner.g.figueiredo@gmail.com>
2021-07-06 17:33:47 +0100

    Create Migration from models

---
+++ Migrations/20210706163307_Create Migration from models.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GarageManagementSystem.Migrations
+{
+    public partial class CreateMigrationfrommodels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
