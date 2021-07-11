25393c6da92eab3b7f1a12faac3b7ddbe791a6ff
kennergf <kenner.g.figueiredo@gmail.com>
2021-07-11 15:50:10 +0100

    Added hour to the day available for booking

--- Services/BookingProvider.cs
+++ Services/BookingProvider.cs
@@ -5,22 +5,47 @@ namespace GarageManagementSystem.Services
 {
     public class BookingProvider
     {
+        /// <summary>
+        /// Day that the booking for next week is made available
+        /// </summary>
         private const DayOfWeek OPENNEXTWEEK = DayOfWeek.Thursday;
+
+        /// <summary>
+        /// Day of the week the garage is closed
+        /// </summary>
         private const DayOfWeek DAYGARAGECLOSED = DayOfWeek.Sunday;
+
+        /// <summary>
+        /// Time of the day garage opens
+        /// </summary>
+        private const int TIMEGARAGEOPEN = 9;
+
+        /// <summary>
+        /// Duration of the work shift
+        /// </summary>
+        private const int WORKSHIFTDURATION = 8;
 
         public List<DateTime> GetAvailabelDates()
         {
             var DatesAvailable = new List<DateTime>();
             var today = DateTime.Today;
-            var targetDate = today;
-            double numberDaysAvailable = today.DayOfWeek >= DayOfWeek.Thursday ? ((double)DAYGARAGECLOSED) + 7 + (7 - (double)today.DayOfWeek) : (7 - (double)today.DayOfWeek);
+            // Start at the time the garage open
+            var targetDate = today.AddHours(TIMEGARAGEOPEN - today.Hour);
+            // Number of days starting from today that will be available to the user
+            double numberDaysAvailable = today.DayOfWeek >= OPENNEXTWEEK ? ((double)DAYGARAGECLOSED) + 7 + (7 - (double)today.DayOfWeek) : (7 - (double)today.DayOfWeek);
             while (targetDate < today.AddDays(numberDaysAvailable))
             {
                 if (targetDate.DayOfWeek != DAYGARAGECLOSED)
                 {
-                    DatesAvailable.Add(targetDate);
+                    // Add Booking spot with one hour between then
+                    for (int i = 0; i < WORKSHIFTDURATION; i++)
+                    {
+                        DatesAvailable.Add(targetDate);
+                        targetDate = targetDate.AddHours(1);
+                    }
                 }
-                targetDate = targetDate.AddDays(1);
+                // Update Date for next day when garage opens
+                targetDate = targetDate.AddHours((24 - targetDate.Hour) + TIMEGARAGEOPEN);
             }
 
             return DatesAvailable;
